19740344a0c63b9ab90e09112222e43ad6b66923
unknown <YY131@ccs.local>
2025-02-05 02:01:38 -0800

    Save Files

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+}); 
+
+//Serve static files in the "public" folder
+app.use(express.static('public'));
+
+//Define a route for the home page
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use(express.json());  //Middleware to parse JSON bodies
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
