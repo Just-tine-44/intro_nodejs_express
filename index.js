0a35bd65002c0051616ffd3e50914c91eb710e60
Justine <justineparaiso44@gmail.com>
2025-02-05 22:47:18 +0800

    Updated index.js in first part

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 });
+
+//middleware to log incoming requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
