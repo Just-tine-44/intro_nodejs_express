ab53af32cd64147795b9a8cd4e5914b592b0ccc4
Justine <justineparaiso44@gmail.com>
2025-02-05 23:40:52 +0800

    Updating the index.html and index.js for the Hands-On Activity: Build a Simple Web Application

--- index.js
+++ index.js
@@ -31,3 +31,17 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
+
+
+//Adding a new route to display a list of items
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
